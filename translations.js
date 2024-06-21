350049a4ee187d9dd2ff7c0e10dc8597f3ca5011
root <daniel.baranek@gmail.com>
2024-06-22 00:38:11 +0200

    translations

--- translations.js
+++ translations.js
@@ -1,6 +1,9 @@
 $(document).on("click", ".flag", function () {
     const lang = $(this).attr("alt") || "cs";
     translation(lang);
+    links(lang);
+    locs(lang);
+    urllang(lang);
 });
 
 function fetchText(path) {
@@ -16,7 +19,7 @@ function fetchText(path) {
 }
 
 function translation(lang) {
-    fetchText('translations.json').then((langData) => {
+    fetchText('trans_texts.json').then((langData) => {
         $.each(langData, function (key, value) {
             $("#" + key).html(value[lang]);
         });
@@ -24,5 +27,48 @@ function translation(lang) {
     }).catch((error) => console.log(`Error while translating: ${error}`));
 }
 
+function links(lang) {
+    fetchText('trans_links.json').then((langData) => {
+        $.each(langData, function (key, value) {
+            $("a#" + key).attr("href", value[lang]["href"]).attr("alt", value[lang]["text"].replace(/<[^>]+>/g, " ")).attr("title", value[lang]["text"].replace(/<[^>]+>/g, " "));
+            $("span#" + key).html(value[lang]["text"]);
+        });
+
+    }).catch((error) => console.log(`Error while translating: ${error}`));
+}
+
+function locs(lang) {
+    fetchText('trans_locs.json').then((locData) => {
+        var locArray = locData.map(function (loc) {
+            return '<a href="locmap.html?loc=' + loc["link"] + '&lang=' + lang + '">' + loc[lang] + '</a>'
+        });
+        var locString = locArray.join(' | ');
+        $('#locs').html(locString);
+    }).catch((error) => console.log(`Error while translating: ${error}`));
+}
+
+function urllang(lang) {
+    var urlParams = new URLSearchParams(location.search);
+    if (urlParams.get('lang')) {
+        var newUrl = location.search.replace(/([\?&])lang=([^&#]*)/, '$1lang=' + lang);
+    } else if (Array.from(urlParams.entries()).length > 0) {
+        var newUrl = location.search + '&lang=' + lang;
+    } else {
+        var newUrl = location.search + '?lang=' + lang;
+    }
+    history.pushState({}, '', newUrl);
+}
+
 // INIT
-translation("cs");
+var urlParams = new URLSearchParams(location.search);
+
+if (urlParams.get('lang')) {
+    var lang = urlParams.get('lang')
+} else {
+    var lang = "cs"
+};
+
+translation(lang);
+links(lang);
+locs(lang);
+urllang(lang);
